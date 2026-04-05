320a1f78452e117e4e087b73bb935b58a748e041
ama_daoud <gamal.daoudyoussouf.etu@univ-lille.fr>
2026-04-05 12:39:58 +0200

    organisation et renommage

--- jhotdraw-core/src/main/java/org/jhotdraw/draw/figure/Attributes.java
+++ jhotdraw-core/src/main/java/org/jhotdraw/draw/figure/Attributes.java
@@ -34,18 +34,21 @@ import org.jhotdraw.draw.AttributeKeys;
 /** implementation of Attribute storage and processing. */
 public final class Attributes {
 
-  private final HashMap<AttributeKey<?>, Object> attributes = new HashMap<>();
+  private final HashMap<AttributeKey<?>, Object> attributesCollection = new HashMap<>();
 
   /**
-   * Forbidden attributes can't be put by the put() operation. They can only be changed by put().
+   * Forbidden attributes can't be put by the put() operation. They can only be
+   * changed by put().
    */
   private HashSet<AttributeKey<?>> forbiddenAttributes;
 
   private AttributeListener listener;
 
-  private Supplier<List<Attributes>> DEPENDENT;
+  private Supplier<List<Attributes>> dependent;
+
   /*
-   * Creates a new instance of Attributes with no listener and no dependent supplier.
+   * Creates a new instance of Attributes with no listener and no dependent
+   * supplier.
    */
   public Attributes() {
     this(null, null);
@@ -53,6 +56,7 @@ public final class Attributes {
 
   /*
    * Creates a new instance of Attributes with the specified listener.
+   *
    * @param listener The attribute listener.
    */
   public Attributes(AttributeListener listener) {
@@ -60,25 +64,38 @@ public final class Attributes {
   }
 
   /*
-   * Creates a new instance of Attributes with the specified listener and dependent supplier.
+   * Creates a new instance of Attributes with the specified listener and
+   * dependent supplier.
+   *
    * @param listener The attribute listener.
+   *
    * @param dependent The supplier for dependent attributes.
    */
   public Attributes(AttributeListener listener, Supplier<List<Attributes>> dependent) {
     this.listener = listener;
-    this.DEPENDENT = dependent == null ? () -> Collections.emptyList() : dependent;
-  }
-  /*
-   * Sets the supplier for dependent attributes. Dependent attributes are attributes of other figures that should be changed when an attribute of this figure is changed. For example, if a figure has a dependent attribute which is the attribute of its parent figure, then when an attribute of this figure is changed, the corresponding attribute of the parent figure should also be changed.
-   * @param dependent The supplier for dependent attributes. If null, no dependent attributes will be used.
-   */
-  public void dependents(Supplier<List<Attributes>> dependent) {
-    this.DEPENDENT = dependent == null ? () -> Collections.emptyList() : dependent;
+    this.dependent = dependent == null ? () -> Collections.emptyList() : dependent;
   }
 
   /*
-   * Enables or disables an attribute for this figure to be processed. If an attribute is disabled, it can't be set on this figure and won't be included in the attributes of this figure.
+   * Sets the supplier for dependent attributes. Dependent attributes are
+   * attributes of other
+   * figures that should be changed when an attribute of this figure is changed.
+   *
+   * @param dependent The supplier for dependent attributes. If null, no dependent
+   * attributes will be used.
+   */
+  public void dependents(Supplier<List<Attributes>> dependent) {
+    this.dependent = dependent == null ? () -> Collections.emptyList() : dependent;
+  }
+
+  /*
+   * Enables or disables an attribute for this figure to be processed. If an
+   * attribute is disabled,
+   * it can't be set on this figure and won't be included in the attributes of
+   * this figure.
+   *
    * @param key The attribute key to enable or disable.
+   *
    * @param b true to enable the attribute, false to disable it.
    */
   public void setAttributeEnabled(AttributeKey<?> key, boolean b) {
@@ -103,8 +120,11 @@ public final class Attributes {
   }
 
   /**
-   * Copy attribute values from a map of attribute values. This detects also the use of
-   * UserData sub maps and copy only those values and creates a new instance of UserData itself.
+   * Copy attribute values from a map of attribute values. This detects also the
+   * use of
+   * UserData sub maps and copy only those values and creates a new instance of
+   * UserData itself.
+   *
    * @param map
    */
   public void setAttributes(Map<AttributeKey<?>, Object> map) {
@@ -118,21 +138,22 @@ public final class Attributes {
   }
 
   public Map<AttributeKey<?>, Object> getAttributes() {
-    return Collections.unmodifiableMap(attributes);
+    return Collections.unmodifiableMap(attributesCollection);
   }
 
   /**
-   * Gets data which can be used to restore the attributes of the figure after a set has been
+   * Gets data which can be used to restore the attributes of the figure after a
+   * set has been
    * applied to it.
    */
   public Object getAttributesRestoreData() {
-    List<Attributes> dependent = DEPENDENT.get();
-    if (dependent.isEmpty()) {
-      return new HashMap<>(attributes);
+    List<Attributes> dependentList = this.dependent.get();
+    if (dependentList.isEmpty()) {
+      return new HashMap<>(attributesCollection);
     } else {
       List<Map<AttributeKey<?>, Object>> list = new ArrayList<>();
-      list.add(new HashMap<>(attributes));
-      for (Attributes attr : dependent) {
+      list.add(new HashMap<>(attributesCollection));
+      for (Attributes attr : dependentList) {
         list.add(new HashMap<>(attr.getAttributes()));
       }
       return list;
@@ -145,23 +166,27 @@ public final class Attributes {
       List<Map<AttributeKey<?>, Object>> list = (List<Map<AttributeKey<?>, Object>>) restoreData;
       restoreAttributesTo(list.get(0));
       int idx = 1;
-      for (Attributes attr : DEPENDENT.get()) {
+      for (Attributes attr : dependent.get()) {
         attr.restoreAttributesTo(list.get(idx));
         idx++;
       }
     } else {
-      attributes.clear();
+      attributesCollection.clear();
       Map<AttributeKey<?>, Object> restoreDataHashMap = (Map<AttributeKey<?>, Object>) restoreData;
       setAttributes(restoreDataHashMap);
     }
   }
 
   /**
-   * Sets an attribute on the figure and calls {@code attributeChanged} on all registered {@code
+   * Sets an attribute on the figure and calls {@code attributeChanged} on all
+   * registered {@code
    * FigureListener}s if the attribute value has changed.
    *
-   * <p>For efficiency reasons, the drawing is not automatically repainted. If you want the drawing
-   * to be repainted when the attribute is changed, you can either use {@code key.set(figure,
+   * <p>
+   * For efficiency reasons, the drawing is not automatically repainted. If you
+   * want the drawing
+   * to be repainted when the attribute is changed, you can either use
+   * {@code key.set(figure,
    * value); } or
    *
    * <pre>
@@ -174,11 +199,11 @@ public final class Attributes {
    */
   public <T> Attributes set(final AttributeKey<T> key, final T newValue) {
     if (forbiddenAttributes == null || !forbiddenAttributes.contains(key)) {
-      T oldValue = key.put(attributes, newValue);
+      T oldValue = key.put(attributesCollection, newValue);
       fireAttributeChanged(key, oldValue, newValue);
     }
 
-    DEPENDENT.get().forEach(a -> Optional.ofNullable(a).ifPresent(at -> at.set(key, newValue)));
+    dependent.get().forEach(a -> Optional.ofNullable(a).ifPresent(at -> at.set(key, newValue)));
     return this;
   }
 
@@ -186,50 +211,42 @@ public final class Attributes {
    * Gets an attribute from the Figure.
    *
    * @see AttributeKey#get
-   * @return Returns the attribute value. If the Figure does not have an attribute with the
-   *     specified key, returns key.getDefaultValue().
+   * @return Returns the attribute value. If the Figure does not have an attribute
+   *         with the
+   *         specified key, returns key.getDefaultValue().
    */
   public <T> T get(AttributeKey<T> key) {
-    return key.get(attributes);
+    return key.get(attributesCollection);
   }
 
   /**
-   * Gets an attribute from the Figure. Includes this attribute, if it was not there.
+   * Gets an attribute from the Figure. Includes this attribute, if it was not
+   * there.
    *
    * @see AttributeKey#get
-   * @return Returns the attribute value. If the Figure does not have an attribute with the
-   *     specified key, returns key.getDefaultValue().
+   * @return Returns the attribute value. If the Figure does not have an attribute
+   *         with the
+   *         specified key, returns key.getDefaultValue().
    */
   public <T> T getAndInclude(AttributeKey<T> key) {
-    return key.getAndInclude(attributes);
-  }
-
-  public static AttributeKey<?> getAttributeKey(String name) {
-    return AttributeKeys.SUPPORTED_ATTRIBUTES_MAP.get(name);
+    return key.getAndInclude(attributesCollection);
   }
 
   /*
-   * Removes an attribute from the figure and calls {@code attributeChanged} on all registered {@code FigureListener}s if the attribute value has changed.
-   * For efficiency reasons, the drawing is not automatically repainted. If you want the drawing to be repainted when the attribute is changed, you can either use {@code key.remove(figure); } or
-   * <pre>
-   * figure.willChange();
-   * figure.remove(...);
-   * figure.changed();
-   * </pre>
+   * Removes an attribute from the figure and calls {@code attributeChanged} on
+   * all registered
+   * {@code FigureListener}s if the attribute value has changed.
+   *
+   * @param key The attribute key to remove.
+   *
+   * @param <T> The type of the attribute value.
+   *
    * @see AttributeKey#remove
-   **
-  * @param key The attribute key to remove.
-  * @param <T> The type of the attribute value.
-  * @return Returns the old value of the attribute that was removed, or {@code null} if the
-    attribute
-  *
-  was not set before.
-  * @see AttributeKey#remove
-  */
+   */
   public <T> void removeAttribute(AttributeKey<T> key) {
     if (hasAttribute(key)) {
       T oldValue = get(key);
-      attributes.remove(key);
+      attributesCollection.remove(key);
       fireAttributeChanged(key, oldValue, key.getDefaultValue());
     }
   }
@@ -241,18 +258,91 @@ public final class Attributes {
    * @return
    */
   public boolean hasAttribute(AttributeKey<?> key) {
-    return attributes.containsKey(key);
+    return attributesCollection.containsKey(key);
+  }
+
+  public static AttributeKey<?> getAttributeKey(String name) {
+    return AttributeKeys.SUPPORTED_ATTRIBUTES_MAP.get(name);
+  }
+
+  /*
+   * Creates a new instance of Attributes by copying the attributes from the
+   * source Attributes.
+   * The listener and dependent supplier are set to null.
+   *
+   * @param source The source Attributes to copy from.
+   *
+   * @return A new instance of Attributes with the copied attributes and no
+   * listener or dependent supplier.
+   */
+  public static Attributes from(Attributes source) {
+    return from(source, null, null);
+  }
+
+  /*
+   * Creates a new instance of Attributes by copying the attributes from the
+   * source Attributes
+   * and using the specified listener. The dependent supplier is set to null.
+   *
+   * @param source The source Attributes to copy from.
+   *
+   * @param listener The attribute listener for the new Attributes. If null, no
+   * listener will be used.
+   *
+   * @return A new instance of Attributes with the copied attributes and specified
+   * listener.
+   */
+  public static Attributes from(Attributes source, AttributeListener listener) {
+    return from(source, listener, null);
+  }
+
+  /*
+   * Creates a new instance of Attributes by copying the attributes from the
+   * source Attributes
+   * and using the specified listener and dependent supplier.
+   *
+   * @param source The source Attributes to copy from.
+   *
+   * @param listener The attribute listener for the new Attributes. If null, no
+   * listener will be used.
+   *
+   * @param dependent The supplier for dependent attributes for the new
+   * Attributes. If null, no dependent attributes will be used.
+   *
+   * @return A new instance of Attributes with the copied attributes and specified
+   * listener and dependent supplier.
+   */
+  public static Attributes from(
+      Attributes source, AttributeListener listener, Supplier<List<Attributes>> dependent) {
+    Attributes attr = new Attributes(listener, dependent);
+    attr.attributesCollection.putAll(source.attributesCollection);
+    if (source.forbiddenAttributes != null) {
+      attr.forbiddenAttributes = new HashSet<>(source.forbiddenAttributes);
+    }
+    return attr;
+  }
+
+  /*
+   * Creates a supplier for the attributes of a list of figures.
+   *
+   * @param dependent The supplier for the list of figures. If null, an empty list
+   * will be used.
+   *
+   * @return A supplier for the attributes of the figures in the list.
+   */
+  public static Supplier<List<Attributes>> attrSupplier(Supplier<List<Figure>> dependent) {
+    return () ->
+        dependent.get().stream().filter(f -> f != null).map(f -> f.attr()).collect(toList());
   }
 
   /**
    * Fires an attribute changed event to all registered {@code FigureListener}s.
    *
    * @param attribute The attribute that has changed.
-   * @param oldValue The old value of the attribute.
-   * @param newValue The new value of the attribute.
-   * <T> The type of the attribute value.
+   * @param oldValue  The old value of the attribute.
+   * @param newValue  The new value of the attribute.
+   * @param <T>       The type of the attribute value.
    * @see AttributeListener#attributeChanged
-   * @see AttributeKey#remove
    */
   private <T> void fireAttributeChanged(AttributeKey<T> attribute, T oldValue, T newValue) {
     if (listener != null) {
@@ -260,55 +350,15 @@ public final class Attributes {
     }
   }
 
-  /* The AttributeListener interface defines a listener for attribute changes. It has a single method, {@code attributeChanged}, which is called when an attribute of the figure is changed. The method receives the attribute that has changed, the old value of the attribute, and the new value of the attribute. This allows the listener to react to changes in the attributes of the figure, for example by updating the appearance of the figure or by triggering other actions.
+  /*
+   * The AttributeListener interface defines a listener for attribute changes.
+   *
    * @see AttributeKey#put
+   *
    * @see AttributeKey#remove
    */
   @FunctionalInterface
   public static interface AttributeListener {
     <T> void attributeChanged(AttributeKey<T> attribute, T oldValue, T newValue);
-  }
-  /* Creates a new instance of Attributes by copying the attributes from the source Attributes. The listener and dependent supplier are set to null.
-   * @param source The source Attributes to copy from.
-   * @return A new instance of Attributes with the copied attributes and no listener or dependent supplier.
-   */
-
-  public static Attributes from(Attributes source) {
-    return from(source, null, null);
-  }
-
-  /* Creates a new instance of Attributes by copying the attributes from the source Attributes and using the specified listener. The dependent supplier is set to null.
-   * @param source The source Attributes to copy from.
-   * @param listener The attribute listener for the new Attributes. If null, no listener will be used.
-   * @return A new instance of Attributes with the copied attributes and specified listener.
-   */
-
-  public static Attributes from(Attributes source, AttributeListener listener) {
-    return from(source, listener, null);
-  }
-
-  /* Creates a new instance of Attributes by copying the attributes from the source Attributes and using the specified listener and dependent supplier.
-   * @param source The source Attributes to copy from.
-   * @param listener The attribute listener for the new Attributes. If null, no listener will be used.
-   * @param dependent The supplier for dependent attributes for the new Attributes. If null, no dependent attributes will be used.
-   * @return A new instance of Attributes with the copied attributes and specified listener and dependent supplier.
-   */
-  public static Attributes from(
-      Attributes source, AttributeListener listener, Supplier<List<Attributes>> dependent) {
-    Attributes attr = new Attributes(listener, dependent);
-    attr.attributes.putAll(source.attributes);
-    if (source.forbiddenAttributes != null) {
-      attr.forbiddenAttributes = new HashSet<>(source.forbiddenAttributes);
-    }
-    return attr;
-  }
-
-  /* Creates a supplier for the attributes of a list of figures. The supplier returns the list of attributes of the figures in the list. If a figure in the list is null, it is ignored.
-   * @param dependent The supplier for the list of figures. If null, an empty list will be used.
-   * @return A supplier for the attributes of the figures in the list.
-   */
-  public static Supplier<List<Attributes>> attrSupplier(Supplier<List<Figure>> dependent) {
-    return () ->
-        dependent.get().stream().filter(f -> f != null).map(f -> f.attr()).collect(toList());
   }
 }
